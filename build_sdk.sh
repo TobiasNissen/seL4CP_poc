5a5efc72c2e34366399ec1c3ca5225bdd75b96ff
TobiasNissen <tobias.nissen@gmail.com>
2022-11-15 13:29:21 +0100

    Added build script

---
+++ build_sdk.sh
@@ -0,0 +1,1 @@
+(cd ../sel4cp && /home/ttn/Desktop/sel4cp/pyenv/bin/python /home/ttn/Desktop/sel4cp/build_sdk.py --sel4=/home/ttn/Desktop/seL4) && rm -rf /home/ttn/Desktop/seL4CP_poc/sel4cp-sdk-1.2.6 && cp -r /home/ttn/Desktop/sel4cp/release/sel4cp-sdk-1.2.6 /home/ttn/Desktop/seL4CP_poc/
